c5c4e220dae8fae5197c3ecb312ee2ab09207f5e
JatoMixo <jatomixo@proton.me>
2024-11-02 16:45:22 +0100

    holy shit it compiles now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
+#![no_std]
+#![no_main]
 
+use esp_println::println;
+use esp_backtrace as _;
+use esp_hal::{
+    clock::ClockControl,
+    delay::Delay,
+    gpio::{Io, Level, Output},
+    peripherals::Peripherals,
+    prelude::*,
+    system::SystemControl,
+};
+
+fn main() -> () {
+
+    let peripherals = Peripherals::take();
+    let system = SystemControl::new(peripherals.SYSTEM);
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
+
+    let delay = Delay::new(&clocks);
+
+    loop {
+
+    }
 }
